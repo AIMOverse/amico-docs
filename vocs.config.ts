6ef1fb01dc2f1c8502aa9f8c8912c7cc3d1cbe16
takasaki404 <takasaki@aimoverse.xyz>
2025-01-21 21:15:30 +0800

    migrate to vocs docs

---
+++ vocs.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "Amico Docs",
+  rootDir: ".",
+  sidebar: {
+    "/docs/": [
+      {
+        text: "Introduction",
+        link: "/docs/introduction",
+      },
+      {
+        text: "Getting Started",
+        link: "/docs/getting-started",
+      },
+      {
+        text: "Packages",
+        collapsed: true,
+        items: [
+          {
+            text: "Overview",
+            link: "/docs/packages/overview",
+          },
+          {
+            text: "Core Library",
+            link: "/docs/packages/amico-core",
+          },
+          {
+            text: "Amico Runtime",
+            link: "/docs/packages/amico",
+          },
+        ],
+      },
+    ],
+  },
+  topNav: [
+    {
+      text: "Documentation",
+      link: "/docs/introduction",
+      match: "/docs/",
+    },
+    {
+      text: "Tutorials",
+      link: "/tutorials",
+      match: "/tutorials/",
+    }
+  ]
+});
