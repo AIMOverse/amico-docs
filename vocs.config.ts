419baa71c056d105b2a5bcbc474bb0534fee1388
takasaki404 <takasaki@aimoverse.xyz>
2025-02-04 20:26:31 +0800

    update docs

--- vocs.config.ts
+++ vocs.config.ts
@@ -15,12 +15,21 @@ export default defineConfig({
         link: `${docsPath}/introduction`,
       },
       {
-        text: "Getting Started",
-        link: `${docsPath}/getting-started`,
-      },
-      {
         text: "Contributing",
         link: `${docsPath}/contributing`,
+      },
+      {
+        text: "Getting Started",
+        items: [
+          {
+            text: "Running Amico Instance",
+            link: `${docsPath}/getting-started/running-instance`,
+          },
+          {
+            text: "Plugin Development",
+            link: `${docsPath}/getting-started/plugin-dev`,
+          },
+        ]
       },
       {
         text: "Packages",
